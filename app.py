d023a2c0a4018e0e22b2d78a433c83f7faccfe21
adityamukherjee43-beep <adityamukherjee43@gmail.com>
2026-06-01 02:06:44 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,23 @@
+rom fastapi import FastAPI
+from fastapi.responses import FileResponse
+import edge_tts
+import uuid
+
+app = FastAPI()
+
+@app.get("/tts")
+async def tts(text: str):
+    filename = f"{uuid.uuid4()}.mp3"
+
+    communicate = edge_tts.Communicate(
+        text,
+        voice="en-IN-PrabhatNeural"
+    )
+
+    await communicate.save(filename)
+
+    return FileResponse(
+        filename,
+        media_type="audio/mpeg",
+        filename="speech.mp3"
+    )
